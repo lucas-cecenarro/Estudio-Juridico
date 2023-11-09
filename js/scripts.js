74db9b95427824c012b86c5c59ea75bb11bda86c
lucas-cecenarro <147412158+lucas-cecenarro@users.noreply.github.com>
2023-11-09 11:03:16 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Obtén el elemento del menú laboral y el menú desplegable
+const laboralMenu = document.getElementById("laboral-menu");
+const submenu = laboralMenu.querySelector("submenu");
+
+// Agrega un evento de clic al elemento del menú laboral
+laboralMenu.addEventListener("click", function () {
+    // Toggle (mostrar/ocultar) el menú desplegable al hacer clic en "Laboral"
+    if (submenu.style.maxHeight) {
+        submenu.style.maxHeight = null;
+    } else {
+        submenu.style.maxHeight = submenu.scrollHeight + "px";
+    }
+});
